d1fd5ff4c896bc998d98d1231ffe17de98e3a566
Gabriel Santos <o.gabriel@aluno.ufabc.edu.br>
2022-06-26 12:33:11 -0300

    get relative path of image

--- script/raft.py
+++ script/raft.py
@@ -1,12 +1,27 @@
 import pyautogui as pg
 import time
+import os
+from re import search
+
+# get actual path
+script_path = path_script = os.path.abspath(__file__)
+
+#move to img folder 'n get
+os.chdir('img')
+
+img_path = os.getcwd()
 
 while True:
-    pescou = pg.locateOnScreen("lmb.png", confidence=0.9)
+    #region caught in variable pescou without region
+    pescou = pg.locateOnScreen(os.path.join(img_path,  'lmb.png') , confidence=0.8299, region=(1250, 441, 59, 41))
+
+    print(type(pescou))
+
     if pescou != None:
         pg.click()
         time.sleep(1)
         pg.click()
+        time.sleep(0.125)
         continue
     else:
         continue
